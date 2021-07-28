cb8201890211f3793b11969f85c6bba7f4df35af
SageblattD <s2931713@gmail.com>
2021-07-28 13:59:10 +0300

    Navigation Rebuild

--- NavigationService.cpp
+++ NavigationService.cpp
@@ -3,160 +3,343 @@
 //
 
 #include "NavigationService.h"
-//double(TheArk::get_instance()->getPopulation()->getAllClassification()[4].size())
 
-NavigationService::NavigationService():State(100),staff(200),
-resources(static_cast<unsigned int>(0.2*(TheArk::get_instance()->getResources()->getComponents()+TheArk::get_instance()->getResources()->getConsumables()))),
-need_staff(200),
-need_resources(static_cast<unsigned int>(0.2*(TheArk::get_instance()->getResources()->getComponents()+TheArk::get_instance()->getResources()->getConsumables()))),
-stage(0),lost_years(0)
+// NavigationBlock implementations
+unsigned short NavigationBlock::getDefaultEfficiency(NavigationDevices type)
 {
+    switch (type) {
+        case INS:
+            return 15;
 
+        case OPTICAL:
+            return 25;
+
+        case COMP:
+            return 35;
+
+        case RADIO:
+            return 20;
+
+        case MAGNETIC:
+            return 5;
+
+        case NavDevAMOUNT:
+            cout << "ERROR IN getDefaultEfficiency" << endl;
+    }
+    return -1;
 }
 
-void NavigationService::process_accident(AccidentSeverity as){int k=0;
-    if(AccidentSeverity::NEGLIGIBLE == as){
-        k = 1;
-
-
-    }
-    if(AccidentSeverity::LIGHT == as){
-        k = 2 ;
-
-
-    }
-    if(AccidentSeverity::MEDIUM == as){
-        k = 3 ;
-
-
-    }
-    if(AccidentSeverity::SEVERE == as){
-        k = 4;
-
-
-    }
-    if(AccidentSeverity::DISASTROUS == as){
-        k = 5;
-
-
-    }
-    if(AccidentSeverity::CATASTROPHIC == as){
-        k = 6;
-
-
-    }
-    this->lost_years += k*(double(TheArk::get_instance()->getYearsTotal()-TheArk::get_instance()->getCurrentYear())/125);
-    this->changeResources(-k * TheArk::get_instance()->getResources()->getComponents()/100);
-    this->changeStaff(k*2);
-
-}
-void NavigationService::process_year(){
-    this->stage = round((TheArk::get_instance()->getCurrentYear()/TheArk::get_instance()->getYearsTotal())*100);
-    int f=0;
-    /*f=rand() % 100 + 1;
-
-    if (f>33 && this->State<95)                   //шанс на удачное сокращение потерянного на инциденты времени
-    {this->lost_years-=rand()%5+1;
-
-
-    }
-    */
-    if (stage !=0 && (stage<5))
-    {this->changeResources(0);
-    }
-    if (stage >= 5 && stage <=20)
-    {
-        this->changeResources(-TheArk::get_instance()->getResources()->getComponents()/1000);
-    }
-    if (stage >= 20 && stage <=80)
-    {
-        this->changeResources(-TheArk::get_instance()->getResources()->getComponents()/100);
-    }
-    if (stage>100)
-        this->stage=100;
-    if (stage >= 80 && stage <=101)
-    {
-        this->changeResources(0);
-    }
-    this->lost_years-=2;
-    if (this->lost_years < 0)
-        this->lost_years==0;
-   // if (this->staff==0) this->State=0;
-    //else
-   // this->State=(100- this->State=(100- (this->lost_years)/TheArk::get_instance()->getYearsTotal()*100);-(this->staff/this->need_staff));
-if (TheArk::get_instance()->getPopulation()->getTotal()==0 || this->State<=0)
-    this->State=0;
-else
-    this->State=100*(1-(this->staff/this->need_staff+lost_years/TheArk::get_instance()->getYearsTotal()));
-
-}
-double NavigationService::getState() {
-    return this->State;
-
-
-}
-
-void NavigationService::setState(double s) {
-    this->staff=round((s - 1) / 100 * need_staff);
-   
-    this->State = (s);
-
-
-}
-
-bool NavigationService:: changeResources(int delta)
-{if (delta < 0)
-    {
-        TheArk::get_instance()->getResources()->setUsedToJunk( delta, Navigation_Service);
-    }
-
-    this->resources += delta;
-    return true;
-
-}
-
-
-bool NavigationService::changeStaff(int delta)
+NavigationBlock::NavigationBlock(NavigationDevices type)
 {
+    state = 0;
+    consumption = 0;
+    efficiency = getDefaultEfficiency(type);
+}
 
-        list<shared_ptr<Human>>& people = TheArk::get_instance()->getPopulation()->getAllClassification()[Navigation_Service];
+unsigned int NavigationBlock::getConsumption() const { return consumption; }
 
-        if (this->staff > delta && delta > 0)
-        {
-            int counter = 0;
+float NavigationBlock::getEfficiency() const { return efficiency; }
 
-            for (auto it = people.begin(); it != people.end(); it++)
-            {
-                while (counter < delta)
-                {
-                    (*it)->setIsAlive(false);
-                    counter++;
-                }
+float NavigationBlock::getState() const { return state; }
 
+void NavigationBlock::setConsumption(unsigned int new_consumption) {
+    consumption = new_consumption;
+}
+
+void NavigationBlock::setEfficiency(float new_efficiency) {
+    efficiency = new_efficiency;
+}
+
+void NavigationBlock::setState(float new_state) {
+    if(new_state <= 100 and new_state >= 0)
+        state = new_state;
+}
+
+void NavigationBlock::changeState(float delta) {
+    state += delta;
+    if (state < 0)
+        state = 0;
+    else if (state > 100)
+        state = 100;
+}
+
+float NavigationBlock::repairDevice(float available_points) {
+    if (available_points < 0) {
+        cout <<"REPAIR DEVICE problem < 0" << endl;
+    }
+    else if (state + available_points > 100) {
+        available_points -= (100 - state);
+        state = 100;
+        return available_points;
+    }
+    else {
+        state += available_points;
+        return 0;
+    }
+}
+
+//
+
+// NavigationService implementations
+
+NavigationService::NavigationService(): staff(0), required_staff(10),
+                                        need_resources(0), years_delta(0)
+{
+    stage = ACCELERATION;
+    time_until_next_stage = ACCELERATION_TIME;
+    next_stage = STABLE;
+
+    for (auto i = 0; i < NavDevAMOUNT; i++)
+        devices[i] =
+                std::make_unique<NavigationBlock>(static_cast<NavigationDevices>(i));
+}
+
+void NavigationService::killStaff(unsigned int victims)
+{
+    list<shared_ptr<Human>>& my_staff =
+            TheArk::get_instance()->getPopulation()->getServiceStaff(Navigation_Service);
+
+    auto it = my_staff.begin();
+    for (int i = 0; i < victims; i++)
+    {
+        (*it)->setIsAlive(false);
+        it++;
+    }
+}
+
+float NavigationService::getRandomFloat(float min, float max)
+{
+    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    static default_random_engine e(seed);
+    uniform_real_distribution<float> d(min, max);
+    return d(e);
+}
+
+int NavigationService::getRandomInt(int min, int max)
+{
+    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    static default_random_engine e(seed);
+    std::uniform_int_distribution<int> d(min, max);
+    return d(e);
+}
+
+// NEGLIGIBLE — lost the way for some time
+// LIGHT — lost some staff and some time
+// MEDIUM — lost some staff and some device's health
+// SEVERE — lost staff, devices' health and time
+// DISASTROUS — lost a lot of staff, devices and probably time
+// CATASTROPHIC — lost almost everything
+void NavigationService::process_accident(AccidentSeverity as)
+{
+    return; // DISABLED UNTIL EMERGENCY IS FIXED
+    unsigned killed_staff = 0;
+    switch (as)
+    {
+        case NEGLIGIBLE:
+            stage = MANEUVER;
+            time_until_next_stage = 5;
+            next_stage = STABLE;
+            TheArk::get_instance()->setYearsTotal(
+                    TheArk::get_instance()->getYearsTotal() + time_until_next_stage);
+
+        case LIGHT:
+            killed_staff = staff / 15;
+            stage = MANEUVER;
+            time_until_next_stage = 10;
+            next_stage = STABLE;
+            TheArk::get_instance()->setYearsTotal(
+                    TheArk::get_instance()->getYearsTotal() + time_until_next_stage);
+
+        case MEDIUM:
+            killed_staff = staff / 10;
+            devices[getRandomInt(0, 5)]->changeState(
+                    getRandomFloat(-10.f, -5.f));
+
+        case SEVERE:
+            killed_staff = staff / 5;
+            stage = MANEUVER;
+            time_until_next_stage = 20;
+            next_stage = STABLE;
+            TheArk::get_instance()->setYearsTotal(
+                    TheArk::get_instance()->getYearsTotal() + time_until_next_stage);
+            devices[getRandomInt(0, 5)]->changeState(
+                    getRandomFloat(-18.f, -10.f));
+
+        case DISASTROUS:
+            killed_staff = staff / 2;
+            for (auto i = 0; i < 4; i++) {
+                devices[getRandomInt(0, 5)]->changeState(
+                        getRandomFloat(-60.f, -30.f));
+            }
+            if (getRandomInt(0, 2) == 1) {
+                stage = MANEUVER;
+                time_until_next_stage = 20;
+                next_stage = STABLE;
+                TheArk::get_instance()->setYearsTotal(
+                        TheArk::get_instance()->getYearsTotal() + time_until_next_stage);
             }
 
-            this->staff -= delta;
+        case CATASTROPHIC:
+            killed_staff = staff * 0.9;
+            for (auto i = 0; i < 6; i++) {
+                devices[getRandomInt(0, 4)]->changeState(
+                        getRandomFloat(-90.f, -60.f));
+            }
+            stage = MANEUVER;
+            time_until_next_stage = 50;
+            next_stage = STABLE;
+            TheArk::get_instance()->setYearsTotal(
+                    TheArk::get_instance()->getYearsTotal() + time_until_next_stage);
+    }
+
+    //cout << "MOGUS" << as << "  " << TheArk::get_instance()->getCurrentYear() << endl;
+    killStaff(killed_staff);
+    staff -= killed_staff;
+}
+
+
+void NavigationService::process_year()
+{
+    // Block for flight stage management
+    time_until_next_stage--;
+    if (not time_until_next_stage) {
+        stage = next_stage;
+
+        if (stage == STABLE) {
+            time_until_next_stage = TheArk::get_instance()->getYearsTotal() -
+                    ACCELERATION_TIME;
+            next_stage = ACCELERATION;
         }
+        else if (stage == ACCELERATION)  {
+            time_until_next_stage = ACCELERATION_TIME;
+            next_stage = STABLE;
+        }
+    }
+    // years_delta section DISABLED UNTIL EMERGENCY IS FIXED
+//    double effective_sum = 0; // Sum for all devices' performance
+//    for (auto & it : devices)
+//        effective_sum += it->getState() * it->getEfficiency() / 10000.f;
+//    years_delta -= effective_sum - 1;
+//    if (years_delta > 3) {
+//        TheArk::get_instance()->setYearsTotal(TheArk::get_instance()->getYearsTotal() + 3);
+//        years_delta -= 3;
+//        cout << "+3 Years" << endl;
+//    } else if (years_delta < -3) {
+//        TheArk::get_instance()->setYearsTotal(TheArk::get_instance()->getYearsTotal() - 3);
+//        years_delta -= -3;
+//        cout << "-3 Years" << endl;
+//    }
+    //cout << years_delta << endl;
+    //
 
-        else
-            if (delta <= 0)
-            this->staff +=delta;
 
-        return true;
+    // Block for staff management
+    if (TheArk::get_instance()->getPopulation()->getUnemployedPeople() >=
+        (required_staff / 10)) {
+        required_staff += TheArk::get_instance()->getPopulation()->getUnemployedPeople() / 15;
+    }
+    else if (staff < std::stoi(
+            TheArk::get_instance()->getInterface()->getGeneral()["Population"]) / 8) {
+        required_staff = std::stoi(
+                TheArk::get_instance()->getInterface()->getGeneral()["Population"]) / 8;
+    }
 
+    staff = TheArk::get_instance()->getPopulation()->getServiceStaff(Navigation_Service).size();
+    //cout <<staff << " - ";
+    //
 
+    // Block for resource management
+    //
+
+    // Block for devices
+     // Efficiency upgrades
+      // Adding points to the device with max state
+    if(staff > std::stoi
+        (TheArk::get_instance()->getInterface()->getGeneral()["Population"]) / 8) {
+        setChangedEfficiency(true);
+        float eff_points = (staff * 100.f) /
+                (std::stof(TheArk::get_instance()->getInterface()->getGeneral()["Population"]) / 8.f)
+                - 100;
+
+// 1       NavigationDevices upgraded_device;
+//        float max_state = 0;
+//        for (auto i = 0; i < NavDevAMOUNT; i++) {
+//            if (devices[i]->getState() > max_state) {
+//                max_state = devices[i]->getState();
+//                upgraded_device = static_cast<NavigationDevices>(i);
+//            }
+//        }
+        for (auto i = 0; i < NavDevAMOUNT; i++) {
+            cout << devices[i]->getState() << "   " << devices[i]->getEfficiency() << endl;
+        }
+        auto upgrd_dev = std::max_element(devices.begin(), devices.end(), CompareDevicesState());// upgraded device
+        //std::cout << upgrd_dev->get()->getState() << " " << upgrd_dev->get()->getEfficiency() << endl;
+//1        devices[upgraded_device]->setEfficiency(devices[upgraded_device]->getEfficiency()
+//        + eff_points);
+        upgrd_dev->get()->setEfficiency(upgrd_dev->get()->getEfficiency() + eff_points);
+    }
+    else if (isChangedEfficiency()) {
+        for (auto i = 0; i < NavDevAMOUNT; i++)
+            devices[i]->setEfficiency(devices[i]->getDefaultEfficiency(
+                    static_cast<NavigationDevices>(i)));
+    }
+    //
+
+    // Repair
+    float CURRENT_YEAR_REPAIR_PERCENT = getRandomFloat(0.7, 1.5)
+            * REPAIR_PERCENT_PER_YEAR
+            * (staff / (std::stof(
+            TheArk::get_instance()->getInterface()->getGeneral()["Population"]) / 8));
+
+    auto rprd_dev = std::max_element(devices.begin(), devices.end(), CompareDevicesEfficiency()); // repaired device
+    CURRENT_YEAR_REPAIR_PERCENT = rprd_dev->get()->repairDevice(CURRENT_YEAR_REPAIR_PERCENT);
+
+    if (CURRENT_YEAR_REPAIR_PERCENT) {
+        rprd_dev = std::min_element(devices.begin(), devices.end(), CompareDevicesState());
+        CURRENT_YEAR_REPAIR_PERCENT = rprd_dev->get()->repairDevice(CURRENT_YEAR_REPAIR_PERCENT);
+
+        auto i = 0;
+        while (i < NavDevAMOUNT and CURRENT_YEAR_REPAIR_PERCENT > 0) {
+            CURRENT_YEAR_REPAIR_PERCENT = devices[i]->repairDevice(CURRENT_YEAR_REPAIR_PERCENT);
+            i++;
+        }
+    }
+    //
+//cout << required_staff << endl;
+//cout << getStaffDemand() << " AMOGUS - " << TheArk::get_instance()->getPopulation()->getUnemployedPeople() << endl;
 }
 
-unsigned int NavigationService::getStaffDemand()
+double NavigationService::getState()
 {
-    //return this->need_staff - this->staff;
-    return (need_staff-staff);
+    float result = 0;
 
-}unsigned int NavigationService::getResourceDemand()
+    for (auto& it : devices)
+        result += it->getState();
+
+    return result / NavDevAMOUNT;
+}
+
+void NavigationService::setState(double s)
 {
-    //return this->need_resources - this->resources;
+    if (not TheArk::get_instance()->getCurrentYear())
+        required_staff = std::stoi(
+                TheArk::get_instance()->getInterface()->getGeneral()["Population"]) / 8;
+    for (auto& it : devices)
+        it->setState(s);
+}
+
+unsigned int NavigationService::getStaffDemand() {
+    return required_staff - staff;
+}
+
+unsigned int NavigationService::getResourceDemand() {
     return 0;
-
 }
 
+bool NavigationService::isChangedEfficiency() const {
+    return changed_efficiency;
+}
 
+void NavigationService::setChangedEfficiency(bool changedEfficiency) {
+    changed_efficiency = changedEfficiency;
+}
--- NavigationService.h
+++ NavigationService.h
@@ -5,43 +5,138 @@
 #ifndef THE_ARK_NAVIGATIONSERVICE_H
 #define THE_ARK_NAVIGATIONSERVICE_H
 
-#include <ctime>
-#include <cmath>
+#include <memory>
+#include <random>
+#include <chrono>
+#include <algorithm>
+#include <iostream>
+
 #include "Service.h"
 #include "TheArk.h"
-#include "MedicalService.h"
-#include "NavigationService.h"
-#include "BiologicalService.h"
-#include "SocialService.h"
-#include "TechnicalService.h"
-#include "Human.h"
-#include "Population.h"
 #include "Resources.h"
+#include "Interface.h"
 
-class NavigationService : public Service {
+using std::unique_ptr;
+using std::default_random_engine;
+using std::uniform_real_distribution;
+using std::cout;
+using std::endl;
+
+enum FlightStage
+{
+    ACCELERATION,
+    STABLE,
+    MANEUVER
+};
+
+enum NavigationDevices
+{
+    INS,
+    OPTICAL,
+    COMP,
+    RADIO,
+    MAGNETIC,
+    NavDevAMOUNT
+};
+
+class NavigationBlock
+{
 private:
-    double State;
-    unsigned int staff;                                 // количество рабочих всего
-    unsigned int resources;
-    unsigned int need_staff;
-    unsigned int need_resources;
-    unsigned int stage;                               //стадия полета(маневрирование, разгон, выход на макс. скорость, торможение) в процентах
-    double lost_years;                                 //сколько потеряно времени из-за происшествий
-public:
-    NavigationService();
- void process_accident(AccidentSeverity as)  override; // каждая служба должна уметь в своих терминах обработать переданную ей аварию
-     void process_year()  override;                        // если у службы есть какая-то личная жизнь, она может заниматься ей тут
-   double getState()  override;                          // каждая служба должна уметь вернуть свое состояние в процентах, посчитав его в своих терминах
-     void setState(double s)  override;                    // функция для инициализации, каждая служба должна уметь получить состояние в процентах и пересчитать  его в своих терминах
+    float state;
+    unsigned int consumption;  // Resources per year
 
-    //unsigned int getResourceDemand();
-    unsigned int getStaffDemand();
-    bool changeResources(int delta);
-    bool changeStaff(int delta);
-    unsigned int getResourceDemand();
-   
+    // Some devices are much more useful than other,
+    // Can change during the flight
+    float efficiency;
+
+public:
+    explicit NavigationBlock(NavigationDevices type);
+
+    // Basic numbers for efficiency of each device
+    unsigned short getDefaultEfficiency(NavigationDevices type);
+
+    float getState() const;
+    unsigned int getConsumption() const;
+    float getEfficiency() const;
+
+    void setState(float new_state);     // Has protection form invalid value
+    void setConsumption(unsigned new_consumption);
+    void setEfficiency(float new_efficiency);
+
+    void changeState(float delta); // Delta can be < 0, has protection form invalid value
+
+    // Points + current state can be > 100, method returns unused points back
+    float repairDevice(float available_points);
 
 };
 
+class CompareDevicesEfficiency{
+public:
+    bool operator() (const unique_ptr<NavigationBlock>& b1,
+                     const unique_ptr<NavigationBlock>& b2)
+    {
+        return b1->getEfficiency() < b2->getEfficiency();
+    }
+};
+
+class CompareDevicesState{
+public:
+    bool operator() (const unique_ptr<NavigationBlock>& b1,
+                     const unique_ptr<NavigationBlock>& b2)
+    {
+        return b1->getState() < b2->getState();
+    }
+};
+
+class NavigationService : public Service {
+private:
+    // Total amount of workers here
+    unsigned int staff;
+
+    // Basically Population / 8, but can be higher
+    // More people — faster repair, higher efficiency
+    unsigned int required_staff;
+
+    // Shows how effective repairing is for a full team
+    const unsigned short REPAIR_PERCENT_PER_YEAR = 20;
+
+    unsigned int need_resources;
+
+    // Stage of the flight
+    FlightStage stage;
+    unsigned short time_until_next_stage;
+    FlightStage next_stage;
+
+    const unsigned short ACCELERATION_TIME = 3;
+
+    array<unique_ptr<NavigationBlock>, NavDevAMOUNT> devices;
+    // Flag for process_year optimization
+    bool changed_efficiency = false;
+
+    // Shows gained or lost time due to the work
+    // of devices, < 0 means faster flight
+    float years_delta;
+
+public:
+    NavigationService();
+
+    void process_accident(AccidentSeverity as)  override; // каждая служба должна уметь в своих терминах обработать переданную ей аварию
+    void process_year()  override;                        // если у службы есть какая-то личная жизнь, она может заниматься ей тут
+    double getState()  override;                          // каждая служба должна уметь вернуть свое состояние в процентах, посчитав его в своих терминах
+    void setState(double s)  override;                    // функция для инициализации, каждая служба должна уметь получить состояние в процентах и пересчитать  его в своих терминах
+
+    unsigned int getStaffDemand() override;
+    unsigned int getResourceDemand() override;
+
+    // Kills some of staff. Victims HAS to be less than staff!
+    void killStaff(unsigned victims);
+
+    // Random numbers generators
+    float getRandomFloat(float min, float max);
+    int getRandomInt(int min, int max);
+
+    bool isChangedEfficiency() const;
+    void setChangedEfficiency(bool changedEfficiency);
+};
 
 #endif //THE_ARK_NAVIGATIONSERVICE_H
