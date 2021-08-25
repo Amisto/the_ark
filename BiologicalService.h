e9c8674e5763074401a3774ce4513d0b467d0e2e
DvornikES <69252174+DvornikES@users.noreply.github.com>
2021-08-25 20:19:09 +0300

    Update BiologicalService.h

--- BiologicalService.h
+++ BiologicalService.h
@@ -1,7 +1,3 @@
-//
-// Created by Amisto on 4/2/2021.
-//
-
 #ifndef THE_ARK_BIOLOGICALSERVICE_H
 #define THE_ARK_BIOLOGICALSERVICE_H
 
@@ -13,39 +9,41 @@ private:
 public:
     BiologicalService();
 
-    void process_accident(AccidentSeverity as) override;    // каждая служба должна уметь в своих терминах обработать переданную ей аварию
+    void process_accident(AccidentSeverity as) override;   
     void
-    process_year() override {};                       // если у службы есть какая-то личная жизнь, она может заниматься ей тут
+    process_year() override {};                       
     double
-    getState() override;                             // каждая служба должна уметь вернуть свое состояние в процентах, посчитав его в своих терминах
-    void setState(double s) override;                       // функция для инициализации, каждая служба должна уметь получить состояние в процентах и пересчитать  его в своих терминах
+    getState() override;                             
+    void setState(double s) override;                    
     unsigned int getResourceDemand() override {return 0;};
     unsigned int getStaffDemand() override {return 0;};
     unsigned int returnJunk() override {return 0;};
+    class BioRes {
+    private:
+        int k, mass, junk, worker_0;
+        double hp, KPD;
+    public:
+        BioRes(unsigned int start_mass, double KPD_e, int worker_optimum, int max_profit, double health_of_system,
+               int consume_for1);
+
+        void RecycleRes(int worker, int Energy, int people);
+
+        int profit(int worker);
+
+        int GetResource();
+
+        double GetJunk();
+
+        double Gethp();
+
+        void Sethp(double health_of_system);
+
+        int GetPeople(int worker);
+    };
+
 };
 
-class BioRes {
-private:
-    int k, mass, junk, worker_0;
-    double hp, KPD;
-public:
-    BioRes(unsigned int start_mass, double KPD_e, int worker_optimum, int max_profit, double health_of_system,
-           int consume_for1);
 
-    void RecycleRes(int worker, int Energy, int people);
-
-    int profit(int worker);
-
-    int GetResource();
-
-    double GetJunk();
-
-    double Gethp();
-
-    void Sethp(double health_of_system);
-
-    int GetPeople(int worker);
-};
 
 struct coord;
 
